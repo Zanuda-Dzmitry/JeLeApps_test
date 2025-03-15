bdab6789e2eba317c710b8bfad2904c28908f18f
Dzmitry_Shaviaka <krayzerkyn@gmail.com>
2025-03-15 12:31:34 +0300

    first commit

---
+++ app.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="wrapper">
+    <div class="card"></div>
+  </div>
+</template>
+
+<style scoped>
+.wrapper {
+  background-color: #f5f5f5;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.card {
+  width: 20rem;
+  height: 20rem;
+  background-color: black;
+  border-radius: 5px;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
+  transition: transform 0.3s ease;
+}
+</style>
